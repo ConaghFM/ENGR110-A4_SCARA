30f54f89974f9a01fce278ac9af17f8e6f119e5f
Conagh <c.fitzgerald-mansell@live.com>
2017-09-26 13:36:28 +1300

    Applied matlab syntax
    I think

--- xyToLinkageAngles.m
+++ xyToLinkageAngles.m
@@ -1,23 +1,31 @@
-xyToTheta1(Xt, Yt)
+function [theta1, theta2] = xyToLinkageAngles(Xt, Yt)
 
-//Constants:
-int R
-int Xm1
-int Ym1
+%Constants:
+R =
+Xm1 =
+Ym1 =
 
-//  Direct Kinematics:
-Xc = ((Xt-Xm1))
+%Direct Kinematics:
+Xc = ((Xt-Xm1));
 
-//  Adjacent
-int AbC = (sqrt(Xt-Xm1)^2 + (Yt-Ym1)^2)/2.00
-singularity check: if (AbC > R)
+    %Adjacent
+int AbC = (sqrt(Xt-Xm1)^2 + (Yt-Ym1)^2)/2.00;
+if (AbC > R)
+    singularity()
+end
 
-//  Opposite
-int n = sqrt(AbC^2 - R^2)
+    %Opposite
+n = sqrt(AbC^2 - R^2);
 
-thetaB = sin^-1(n, R)
-thetaA = sin^-1((Yx-Ym1)/(Xc-Ym1))   //assumes px are counted from top of frame
-if (invertedFlag) theta = thetaA - thetaB
-else theta = thetaA + thetaB
+thetaB = arcsin(n, R);
+thetaA = arcsin((Yx-Ym1)/(Xc-Ym1));   %assumes px are counted from top of frame
 
-return theta
+if (invertedL)
+    theta1 = thetaA - thetaB;
+else theta1 = thetaA + thetaB;
+end
+
+%Inverse Kinematics:
+theta2 = theta1ToTheta2(theta1, Xt, Yt);
+
+return [theta1, theta2]
