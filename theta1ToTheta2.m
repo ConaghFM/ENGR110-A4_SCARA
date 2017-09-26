0e43e26bbd5bb1e8782ad559d96a822b30ae9f82
Conagh <c.fitzgerald-mansell@live.com>
2017-09-26 13:27:57 +1300

    Applied matlab syntax
    I think

--- theta1ToTheta2.m
+++ theta1ToTheta2.m
@@ -1,28 +1,28 @@
-theta1ToTheta2(theta1, Xt, Yt)
+function theta2 = theta1ToTheta2(theta1, Xt, Yt)
 
-//Constants:
-int R
-int Xm2
-int Ym2
+%Constants:
+R = 
+Xm2
+Ym2
 
-int Xc = (Xm2-Xt)/2+Xt
-int Yc = (Ym2-Yt)/2+Yt
-int AbC = (sqrt(Xm2-Xt)^2+(Ym2-Yt)^2)/2
-if (AbC > R)
-    singularity()
+Xc = (Xm2-Xt)/2+Xt;
+Yc = (Ym2-Yt)/2+Yt;
+AbC = (sqrt(Xm2-Xt)^2+(Ym2-Yt)^2)/2;
+if (AbC > R) %singularity
+    return;
+end
 
-int n = sqrt(R^2-AbC^2)
-int beta = arctan((Ym2-Yt)/(Xm2-Xt))
+n = sqrt(R^2-AbC^2);
+beta = arctan((Ym2-Yt)/(Xm2-Xt));
 
-if (invertedR){
-    Xj = Xc - n * Cos((Pi/2.00)-beta)
-    Yj = Yc - n * Sin((Pi/2.00)-beta)
-}
-else {
-    Xj = Xc + n * Cos((Pi/2.00)-beta)
-    Yj = Yc + n * Sin((Pi/2.00)-beta)
-}
+if (invertedR)
+    Xj = Xc - n * Cos((Pi/2.00)-beta);
+    Yj = Yc - n * Sin((Pi/2.00)-beta);
+else 
+    Xj = Xc + n * Cos((Pi/2.00)-beta);
+    Yj = Yc + n * Sin((Pi/2.00)-beta);
+end
 
-theta2 = arctan2((Xj-Xm2), (Yj-Ym2))
+theta2 = arctan2((Xj-Xm2), (Yj-Ym2));
 
-return theta2
+return theta2;
