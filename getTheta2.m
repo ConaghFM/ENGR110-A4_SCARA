7ae077cdc39750b9250607a69001ce54c31afa96
Conagh <c.fitzgerald-mansell@live.com>
2017-09-28 15:35:31 +1300

    Fixed drawline function
    now ouputs array formatted for moni

--- getTheta2.m
+++ getTheta2.m
@@ -6,28 +6,28 @@ Xm2 = 394;
 Ym2 = 445;
 
 %Calculate C cooridinates, halfway point on hypotenuse of T and M2
-Xc = (Xm2-Xt)/2+Xt
-Yc = (Ym2-Yt)/2+Yt
+Xc = (Xm2-Xt)/2+Xt;
+Yc = (Ym2-Yt)/2+Yt;
 
 %Adjacent length from C to T
-AbC = sqrt((Xm2-Xt)^2+(Ym2-Yt)^2)
-AbC = AbC/2.00
+AbC = sqrt((Xm2-Xt).^2+(Ym2-Yt).^2);
+AbC = AbC/2.00;
 
 %if (AbC > R) %singularity
 %    return;
 %end
 
 %Opposite length from C to J2
-n = sqrt(R^2 - AbC^2)
+n = sqrt(R^2 - AbC.^2);
 
-value1 = Xm2-Xt
+value1 = Xm2-Xt;
 if (value1 == 0)
-    value1 = 1
+    value1 = 1;
 end
 
-beta = atan((Ym2-Yt)/value1)
-delta = acos(AbC/R)
-theta2 = (pi/2.0)+beta-delta
+beta = atan((Ym2-Yt)/value1);
+delta = acos(AbC/R);
+theta2 = (pi/2.0)+beta-delta;
 
 %arg1 = Xj-Xm2
 %arg2 = Yj-Ym2
--- xyToLinkageAngles.m
+++ xyToLinkageAngles.m
@@ -10,14 +10,14 @@ Xc = ((Xt-Xm1));
 Yc = ((Yt-Ym1)/2.00 + Ym1);
 
     %Adjacent
-AbC = sqrt((Xt-Xm1)^2 + (Yt-Ym1)^2)
-AbC = AbC/2.00
+AbC = sqrt((Xt-Xm1).^2 + (Yt-Ym1).^2);
+AbC = AbC/2.00;
 if (AbC > R)
     % singularity()
 end
 
     %Opposite
-n = sqrt(R^2 - AbC^2);
+n = sqrt(R^2 - AbC.^2);
 
 thetaB = asin(n/R);
 thetaA = asin((Yc-Ym1)/(Xc-Xm1));   %assumes px are counted from top of frame
