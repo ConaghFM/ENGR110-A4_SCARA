c596195e3ec91d87b5b6bb9dca285cde31fd0ce8
Haydenrolston <hayden.rolston@gmail.com>
2017-10-03 17:38:26 +1300

    Update xyToLinkageAngles.m

--- xyToLinkageAngles.m
+++ xyToLinkageAngles.m
@@ -1,26 +1,27 @@
 function [theta1, theta2] = xyToLinkageAngles(Xt, Yt)
 
 %Constants:
-R = 241;
-Xm1 = 261;
-Ym1 = 448;
+R = 241.0;
+Xm1 = 261.0;
+Ym1 = 448.0;
 
 %Direct Kinematics:
-Xc = ((Xt-Xm1));
+Xc = ((Xt-Xm1)/2.00 + Xm1);
 Yc = ((Yt-Ym1)/2.00 + Ym1);
 
     %Adjacent
 AbC = sqrt((Xt-Xm1).^2 + (Yt-Ym1).^2);
 AbC = AbC/2.00;
 if (AbC > R)
-    % singularity()
+    AbC = R;
+    disp("T out of bounds");
 end
 
     %Opposite
-n = sqrt(R^2 - AbC.^2);
+n = sqrt(R.^2 - AbC.^2);
 
 thetaB = asin(n/R);
-thetaA = asin((Yc-Ym1)/(Xc-Xm1));   %assumes px are counted from top of frame
+thetaA = atan((Yc-Ym1)/(Xc-Xm1));   %assumes px are counted from top of frame
 
 if (0) %to be fixed
     theta1 = thetaA - thetaB;
