5abdca3d07c5fc5fac886f86b21ba21c49ba4593
Haydenrolston <hayden.rolston@gmail.com>
2017-10-03 17:28:49 +1300

    bug fixes

--- getTheta2.m
+++ getTheta2.m
@@ -1,13 +1,13 @@
 function theta2 = getTheta2(Xt, Yt)
 
 %Constants:
-R = 241;
-Xm2 = 394;
-Ym2 = 445;
+R = 241.0;
+Xm2 = 394.0;
+Ym2 = 445.0;
 
 %Calculate C cooridinates, halfway point on hypotenuse of T and M2
-Xc = (Xm2-Xt)/2+Xt;
-Yc = (Ym2-Yt)/2+Yt;
+Xc = (Xm2-Xt)/2.0+Xt;
+Yc = (Ym2-Yt)/2.0+Yt;
 
 %Adjacent length from C to T
 AbC = sqrt((Xm2-Xt).^2+(Ym2-Yt).^2);
@@ -18,7 +18,7 @@ AbC = AbC/2.00;
 %end
 
 %Opposite length from C to J2
-n = sqrt(R^2 - AbC.^2);
+n = sqrt(R.^2 - AbC.^2);
 
 value1 = Xm2-Xt;
 if (value1 == 0)
@@ -27,7 +27,7 @@ end
 
 beta = atan((Ym2-Yt)/value1);
 delta = acos(AbC/R);
-theta2 = (pi/2.0)+beta-delta;
+theta2 = pi-beta-delta;
 
 %arg1 = Xj-Xm2
 %arg2 = Yj-Ym2
