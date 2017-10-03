e93f2460962224f2602b096acfeee0d6720912df
Conagh <c.fitzgerald-mansell@live.com>
2017-10-03 18:32:20 +1300

    1830_03_10_2017
    HNNNNNG

--- a2pwm.m
+++ a2pwm.m
@@ -5,7 +5,8 @@ function [motor1, motor2] = a2pwm(theta1, theta2)
 %motor1 = (theta1 - 2.1*10^2) / -0.097
 %motor2 = (theta2 - 1.7*10^2)/ -0.08
 %motor1 = (-10 * theta2 + 2*10^3)
-motor1 = round(-10 * (theta1 - 200));
-motor2 = round(-(500/47) * (theta2 - 250));
+
+motor1 = round(-10 * (theta1 - 250));
+motor2 = round(-10 * (theta2 - 220));
 
 end
--- xyToLinkageAngles.m
+++ xyToLinkageAngles.m
@@ -6,27 +6,29 @@ Xm1 = 261.0;
 Ym1 = 448.0;
 
 %Direct Kinematics:
-Xc = ((Xt-Xm1)/2.00 + Xm1);
-Yc = ((Yt-Ym1)/2.00 + Ym1);
+%Xc = ((Xt-Xm1)/2.00 + Xm1);
+%Yc = ((Yt-Ym1)/2.00 + Ym1);
 
     %Adjacent
 AbC = sqrt((Xt-Xm1).^2 + (Yt-Ym1).^2);
 AbC = AbC/2.00;
 if (AbC > R)
     AbC = R;
-    disp(T out of bounds);
+    disp('T out of bounds');
 end
 
     %Opposite
 n = sqrt(R.^2 - AbC.^2);
 
 thetaB = asin(n/R);
-thetaA = atan((Yc-Ym1)/(Xc-Xm1));   %assumes px are counted from top of frame
+thetaA = atan((Yt-Ym1)/(Xt-Xm1));   %assumes px are counted from top of frame
 
-if (0) %to be fixed
-    theta1 = thetaA - thetaB;
-else theta1 = radtodeg(thetaA + thetaB);
-end
+%if (0) %to be fixed
+%    theta1 = thetaA - thetaB;
+%else 
+%end
+
+theta1 = radtodeg(thetaA + thetaB);
 
 %Inverse Kinematics:
 theta2 = radtodeg(getTheta2(Xt, Yt));
